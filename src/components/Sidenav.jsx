7ec5a5d701c1d665f2cb0a2a6b47b9ab4a2ae81a
igd93 <102153262+igd93@users.noreply.github.com>
2023-10-23 17:13:34 +0200

    added logo, adding menu, mobile menu and a hamburger menu to navbar component

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,10 +1,43 @@
 import React from "react";
-import {AiOutlineMenu} from 'react-icons/ai';
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/computer-logos-4.gif";
 
 function Sidenav() {
-  return (<div>
-    <AiOutlineMenu />
-  </div>);
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} style={{ width: "70px" }} />
+      </div>
+      {/*Menu */}
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contacts</li>
+        </ul>
+      </div>
+      {/*Hamburger*/}
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      {/*Mobile Menu*/}
+      <div className="hidden">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contacts</li>
+        </ul>
+      </div>
+
+      {/*Social Icons*/}
+      <div></div>
+    </div>
+  );
 }
 
 export default Sidenav;
