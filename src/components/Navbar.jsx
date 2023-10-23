f84f82c39190e38deeddf06283a533afb5b8ecff
igd93 <102153262+igd93@users.noreply.github.com>
2023-10-23 17:45:05 +0200

    Added stylings and ternary operators to navigate between mobile and desktop menus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/computer-logos-4.gif";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
@@ -19,15 +22,18 @@ const Navbar = () => {
       </ul>
 
       {/*Hamburger*/}
-      <div className="md:hidden z-10">
-        <FaBars />
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/*Mobile Menu*/}
 
       <ul
-        className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] 
-      flex flex-col justify-center items-center"
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
       >
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
