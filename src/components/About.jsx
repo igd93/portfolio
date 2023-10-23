6a9f40dedd9c1df85dd7e49e65d75687a468ceff
igd93 <102153262+igd93@users.noreply.github.com>
2023-10-23 20:23:25 +0200

    populating the about section; fixed the padding to the center

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,22 @@ import React from "react";
 const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-400">
-      <div className="flex- flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full px-4 grid-cols gap-8">
-          <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-red-500">About</p>
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-left pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-red-500">
+              About
+            </p>
           </div>
           <div></div>
+          </div>
+          <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+            <div>
+              <p>
+                Hi. I am Igor Drozhilkin. Nice to meet you. Please take a look
+                around.
+              </p>
+            </div>          
         </div>
       </div>
     </div>
