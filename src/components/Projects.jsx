8fa2a89d708f8cfbbdbcb2d16d30b55d544fccfd
igd93 <102153262+igd93@users.noreply.github.com>
2023-10-24 18:54:35 +0200

    fixed oveflow issue with Skills  component on mobile. Added Projects component with hover effect

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,174 @@ import FoodDelivery from "../assets/FoodDelivery.png";
 
 const Projects = () => {
   return (
-    <div></div>
+    <div className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-red-500">
+            Projects
+          </p>
+          <p className="py-6">Here are some of my recent projects: </p>
+        </div>
+
+        {/* Grid Container*/}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Food Delivery Cart Item */}
+          <div
+            style={{ backgroundImage: `url(${FoodDelivery})` }}
+            className="shadow-lg shadow-black-300 group container 
+          rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-80 ">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Food Delivery App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/*eCommerce Cart Item */}
+          <div
+            style={{ backgroundImage: `url(${Ecommerce})` }}
+            className="shadow-lg shadow-black-300 group container 
+          rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-80 ">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                eCommerce App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* the World App */}
+          <div
+            style={{ backgroundImage: `url(${World})` }}
+            className="shadow-lg shadow-black-300 group container 
+          rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-80 ">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                World App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Data React Demo */}
+
+          <div
+            style={{ backgroundImage: `url(${DataDemo})` }}
+            className="shadow-lg shadow-black-300 group container 
+          rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-80 ">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Data Demo App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Repeat several times to look pretty */ }
+          <div
+            style={{ backgroundImage: `url(${FoodDelivery})` }}
+            className="shadow-lg shadow-blue-300 group container 
+          rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-80 ">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Food Delivery App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Ecommerce})` }}
+            className="shadow-lg shadow-blue-300 group container 
+          rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-80 ">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Ecommerce Delivery App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,7 +14,7 @@ import Docker from "../assets/docker-original.svg";
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300 overflow-auto">
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-red-500">
@@ -26,7 +26,7 @@ const Skills = () => {
         </div>
 
         {/* Image container */}
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8 overflow-auto">
           {/* JavaScript*/}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img
