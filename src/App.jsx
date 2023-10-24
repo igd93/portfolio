674577a777867318c000515efc7f102fde9a8752
igd93 <102153262+igd93@users.noreply.github.com>
2023-10-24 16:08:29 +0200

    initialized projects component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 function App() {
   return (
     <div>
-      <Navbar />      
+      <Navbar />
       <Home />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 }
